28a9b0aba43797533cb6f1d8ee6af4043bf65b03
Jasmine <52604018+hiimjasmine00@users.noreply.github.com>
2025-09-03 17:57:01 -0400

    screw it

--- src/main.cpp
+++ src/main.cpp
@@ -14,32 +14,38 @@ class $modify(CommentCell) {
 		}
 	}
 
+	void updateColor(GJComment* comm) {
+		auto color = user_data::get<ccColor3B>(comm, "camila314.comment-color");
+
+		if (color.isOk()) {
+			if (auto node = static_cast<TextArea*>(this->getChildByIDRecursive("comment-text-area"))) {
+				node->colorAllLabels(color.unwrap());
+			}
+
+			if (auto node = static_cast<CCLabelBMFont*>(this->getChildByIDRecursive("comment-text-label"))) {
+				node->setColor(color.unwrap());
+			}
+
+			if (auto node = typeinfo_cast<CCRGBAProtocol*>(this->getChildByIDRecursive("prevter.comment_emojis/comment-text-area"))) {
+				node->setColor(color.unwrap());
+			}
+
+			if (auto node = typeinfo_cast<CCRGBAProtocol*>(this->getChildByIDRecursive("prevter.comment_emojis/comment-text-label"))) {
+				node->setColor(color.unwrap());
+			}
+		}
+	}
+
 	void loadFromComment(GJComment* comm) {
 		CommentCell::loadFromComment(comm);
 
-		this->addEventListener<user_data::CommentFilter>([this](GJComment* comm) {
-			auto color = user_data::get<ccColor3B>(comm, "camila314.comment-color");
-
-			if (color.isOk()) {
-				if (auto node = this->getChildByIDRecursive("comment-text-area")) {
-					reinterpret_cast<TextArea*>(node)->colorAllLabels(color.unwrap());
-				}
-
-				if (auto node = this->getChildByIDRecursive("comment-text-label")) {
-					reinterpret_cast<CCLabelBMFont*>(node)->setColor(color.unwrap());
-				}
-
-				if (auto node = this->getChildByIDRecursive("prevter.comment_emojis/comment-text-area")) {
-					log::info("yeah!!!");
-					typeinfo_cast<CCRGBAProtocol*>(node)->setColor(color.unwrap());
-				}
-
-				if (auto node = this->getChildByIDRecursive("prevter.comment_emojis/comment-text-label")) {
-					log::info("yeah!!!");
-					typeinfo_cast<CCRGBAProtocol*>(node)->setColor(color.unwrap());
-				}
-			}
-		}, comm->m_accountID);
+		if (user_data::contains(comm, "camila314.comment-color")) {
+			updateColor(comm);
+		} else {
+			this->addEventListener<user_data::CommentFilter>([this](GJComment* comm) {
+				updateColor(comm);
+			}, comm->m_accountID);
+		}
 	}
 };
 
@@ -48,9 +54,8 @@ $on_mod(Loaded) {
 	auto col = Mod::get()->getSettingValue<ccColor3B>("comment-color");
 	user_data::upload(col, "camila314.comment-color");
 
-	new EventListener(+[](std::shared_ptr<SettingV3> thing) {
-		auto col = Mod::get()->getSettingValue<ccColor3B>("comment-color");
+	listenForSettingChangesV3<ccColor3B>("comment-color", [](ccColor3B col) {
 		user_data::upload(col, "camila314.comment-color");
-	}, SettingChangedFilterV3(Mod::get(), "comment-color"));
+	});
 
 }
