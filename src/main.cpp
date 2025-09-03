3131a6e71a3185d5bcd02467d5e0a1092f411703
Jasmine <52604018+hiimjasmine00@users.noreply.github.com>
2025-09-03 18:03:14 -0400

    You know what? I won't bother

--- src/main.cpp
+++ src/main.cpp
@@ -18,20 +18,22 @@ class $modify(CommentCell) {
 		auto color = user_data::get<ccColor3B>(comm, "camila314.comment-color");
 
 		if (color.isOk()) {
-			if (auto node = static_cast<TextArea*>(this->getChildByIDRecursive("comment-text-area"))) {
-				node->colorAllLabels(color.unwrap());
+			if (auto node = this->getChildByIDRecursive("comment-text-area")) {
+				reinterpret_cast<TextArea*>(node)->colorAllLabels(color.unwrap());
 			}
 
-			if (auto node = static_cast<CCLabelBMFont*>(this->getChildByIDRecursive("comment-text-label"))) {
-				node->setColor(color.unwrap());
+			if (auto node = this->getChildByIDRecursive("comment-text-label")) {
+				reinterpret_cast<CCLabelBMFont*>(node)->setColor(color.unwrap());
 			}
 
-			if (auto node = typeinfo_cast<CCRGBAProtocol*>(this->getChildByIDRecursive("prevter.comment_emojis/comment-text-area"))) {
-				node->setColor(color.unwrap());
+			if (auto node = this->getChildByIDRecursive("prevter.comment_emojis/comment-text-area")) {
+				log::info("yeah!!!");
+				typeinfo_cast<CCRGBAProtocol*>(node)->setColor(color.unwrap());
 			}
 
-			if (auto node = typeinfo_cast<CCRGBAProtocol*>(this->getChildByIDRecursive("prevter.comment_emojis/comment-text-label"))) {
-				node->setColor(color.unwrap());
+			if (auto node = this->getChildByIDRecursive("prevter.comment_emojis/comment-text-label")) {
+				log::info("yeah!!!");
+				typeinfo_cast<CCRGBAProtocol*>(node)->setColor(color.unwrap());
 			}
 		}
 	}
@@ -54,8 +56,9 @@ $on_mod(Loaded) {
 	auto col = Mod::get()->getSettingValue<ccColor3B>("comment-color");
 	user_data::upload(col, "camila314.comment-color");
 
-	listenForSettingChangesV3<ccColor3B>("comment-color", [](ccColor3B col) {
+	new EventListener(+[](std::shared_ptr<SettingV3> thing) {
+		auto col = Mod::get()->getSettingValue<ccColor3B>("comment-color");
 		user_data::upload(col, "camila314.comment-color");
-	});
+	}, SettingChangedFilterV3(Mod::get(), "comment-color"));
 
 }
